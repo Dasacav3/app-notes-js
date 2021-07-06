162f304f9a1f705e19aea8b01f98f4d9bb2929b3
Dasacav3 <dscv3719@gmail.com>
2021-07-05 20:43:19 -0500

    Configuracion de variables de entorno con env

--- app.js
+++ app.js
@@ -1,29 +1,31 @@
+require('dotenv').config()
+
 const express = require("express");
 const app = express();
-const port = 3000;
 const mongoose = require("mongoose");
 
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+
 // Connection to DB MongoDB Atlas
-const user = "user_root_01";
-const password = "RPIDHMsafNsPUJsf";
-const dbname = "app_notes";
-const uri = `mongodb+srv://${user}:${password}@cluster0.peqdu.mongodb.net/${dbname}?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@cluster0.peqdu.mongodb.net/${process.env.MONGODB_DB}?retryWrites=true&w=majority`;
 
-mongoose.connect(uri, {
-	useNewUrlParser: true, 
-	useUnifiedTopology: true
-})
-	.then( () => console.log("Base de datos conectada"))
-	.catch(e => console.error(e))
-
-app.set('view engine', 'ejs');
-app.set('views', __dirname + '/views');
+mongoose
+	.connect(uri, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
+	.then(() => console.log("Base de datos conectada"))
+	.catch((e) => console.error(e));
 
 app.use(express.static(__dirname + "/public"));
 
-// Rutas web
-app.use('/', require("./routes/notes").router);
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
 
-app.listen(port, () => {
-	console.log(`Listening at http://localhost:${port}`);
+// Rutas web
+app.use("/", require("./routes/notes").router);
+
+app.listen(process.env.PORT, () => {
+	console.log(`Listening at http://localhost:${process.env.PORT}`);
 });
